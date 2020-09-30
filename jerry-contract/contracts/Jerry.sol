374c573a16c67d8074e4824a54d97625efd7b822
Janssen <janssenkuhn@mailbox.org>
2020-09-30 11:51:34 -0600

    chain and contract loaded, working on account to send tx with

--- jerry-contract/contracts/Jerry.sol
+++ jerry-contract/contracts/Jerry.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
+//
 // import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 // import "@openzeppelin/contracts-ethereum-package/contracts/Initializable.sol";
